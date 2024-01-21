b8d2683ce4fcfc959029ae5c51dbbf44a46548e0
Sebastiaan Janssen <sebastiaan@umbraco.com>
2024-01-21 17:53:47 +0100

    LAst bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cultiv.netlify.app/"
-  });
+  site: "https://cultiv.netlify.app/",
+  integrations: [preact()]
+});
